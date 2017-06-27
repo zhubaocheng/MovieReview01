e37bb1017c66f0f632e83f08c46d5d984e991786
zhubaocheng <imzhubaocheng@gmail.com>
2017-06-27 11:24:22 +0800

    join & quit movie action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create,:edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :create,:edit, :destroy, :join, :quit]
   before_action :find_movie_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
@@ -41,6 +41,30 @@ class MoviesController < ApplicationController
     @movie.destroy
     redirect_to movies_path
     flash[:alert] = "删除电影成功！"
+  end
+
+  def join
+    @movie = Movie.find(params[:id])
+
+    if !current_user.is_member_of?(@movie)
+      current_user.join!(@movie)
+      flash[:notice] = "收藏成功！！！"
+    else
+      flash[:warning] = "你已经收藏了《"+@movie.title+"》"
+    end
+    redirect_to movie_path(@movie)
+  end
+
+  def quit
+    @movie = Movie.find(params[:id])
+
+    if current_user.is_member_of?(@movie)
+      current_user.quit!(@movie)
+      flash[:alert] = "已退出收藏《"+@movie.title+"》"
+    else
+      flash[:warning] = "你已经退出收藏《"+@movie.title+"》！ 怎么退出xd"
+    end
+    redirect_to movie_path(@movie)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :movies do
+    member do
+      post :join
+      post :quit
+    end
     resources :reviews
   end
   root 'movies#index'
